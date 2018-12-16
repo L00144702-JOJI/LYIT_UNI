f1e185ac59713ba36bb1b9c34699ee41eba9bd73
L00144702-JOJI <44712003+L00144702-JOJI@users.noreply.github.com>
2018-12-16 02:36:22 +0000

    Update L00144702_Std_Details.java

--- L00144702_Std_Details.java
+++ L00144702_Std_Details.java
@@ -4,9 +4,8 @@ public class L00144702_Std_Details
 {
 	public static void main(String args[])
 	{
-		String Std_Name, Std_DOB;
-		String Std_LNo;
-		integer Std_PNo;
+		String Std_Name, Std_DOB, Std_LNo, Std_Address;
+		Integer Std_PNo;
 		
 		Scanner sc=new Scanner(System.in);
 		
@@ -16,11 +15,14 @@ public class L00144702_Std_Details
 		Std_DOB=sc.nextLine();
 		System.out.println("Student Lnumber:");
 		Std_LNo=sc.nextLine();
+		System.out.println("Student Address:");
+		Std_Address=sc.nextLine();
 		System.out.println("Student Phone No.:");
 		Std_PNo=sc.nextInt();
 		System.out.println("\nStudent Name:"+Std_Name);
 		System.out.println("Student DOB:"+Std_DOB);
 		System.out.println("Student LNumber:"+Std_LNo);
-		System.out.println("Student Phone No.:",+Std_PNo);
+		System.out.println("Student Phone No.:"+Std_PNo);
+		System.out.println("Student Address:"+Std_Address);
 	}
 }
