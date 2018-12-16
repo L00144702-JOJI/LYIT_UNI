787d53c2eb26cc78a40a56c5e1ebb814a3316aa4
L00144702-Joji <L00144702@student.lyit.ie>
2018-12-16 02:26:33 +0000

    L00144702_User_Login.java Exported

---
+++ L00144702_User_Login.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+ 
+public class L00144702_User_Login
+{
+	public static void main(String args[])
+	{
+		String User1="Admin";
+		String Pwd1="Admin123";
+		String User2="Guest";
+		String Pwd2="Guest2";
+		String User;
+		String Pwd;
+			
+		Scanner s=new Scanner(System.in);
+			
+		System.out.println("\nEnter User Name:");
+		User=s.nextLine();
+		System.out.println("Ener Password:");
+		Pwd=s.nextLine();
+		
+		if(User.equals(User1) && (Pwd.equals(Pwd1)))
+		{
+			System.out.println("Welcome Admin");
+		}
+		else if(User.equals(User2) && (Pwd.equals(Pwd2)))
+		{
+			System.out.println("Welcome Guest");
+		}
+		else
+		{
+			System.out.println("Wrong Username and Password");
+		}
+	}
+}
